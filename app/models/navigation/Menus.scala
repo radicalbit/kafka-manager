f27e989ff1d13b7d7b61a46e2a7b738713ec556f
lfili <lorenzo.fili@radicalbit.io>
2017-08-24 07:52:47 +0200

    DISTRO 783 - Remove unnecessary features (#8)
    * DISTRO 795 - Remove preferred replica election menu
    
    * DISTRO 796 - Remove reassign partitions menu
    
    * DISTRO 789 - Remove generate partition assignments
    
    * DISTRO 790 - Remove run partition assignments
    
    * DISTO 793 - Remove reassign partition
    
    * DISTO 794 - Remove manual partition assignments
    
    * DISTRO 816 - Remove add partitions

--- app/models/navigation/Menus.scala
+++ app/models/navigation/Menus.scala
@@ -72,8 +72,6 @@ class Menus(implicit applicationFeatures: ApplicationFeatures) {
       clusterMenu(cluster),
       brokersMenu(cluster),
       topicMenu(cluster),
-      preferredReplicaElectionMenu(cluster),
-      reassignPartitionsMenu(cluster),
       consumersMenu(cluster),
       logKafkaMenu(cluster, clusterFeatures)
     ).flatten
